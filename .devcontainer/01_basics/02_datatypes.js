26a75b323304f4085b707078a300f1aa86fec08e
Rishabh Pandey <rishabh22154101@akgec.ac.in>
2025-11-24 20:13:21 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js as newer version
+//alert(3+3) we are using node.js not browser
+
+//semi columns are avoided just with "use strictly"
+let name = "rishabh"
+let age = 22 
+let isaLoggedIn = false
+//null REPRESENTATION OF EMTY VALUE is a speacial data type in JS
+//UNDEFINED IS JUST NOT DEFINED
+// NULL IS A STAND ALONE VALUE WHILE UNDEFINED MEANS DATATYPE HAS NO VALUE ASSIGNED
+
+
+//-------
+/* symbols-> unique
+
+*/
+console.log (typeof null)//returns object
+console.log(typeof undefined) //returns undefined
+
