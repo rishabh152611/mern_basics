5f2c2f3bed0fc607c88c1f474a29bf89aaaec63e
Rishabh Pandey <rishabh22154101@akgec.ac.in>
2025-11-24 19:48:54 +0000

    day2wellused

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,7 +2,15 @@ const accountId = 144553 //unchangable
 let accountEmail = "rishi@69"//
 var accountPassword ="12345"
 accountCity = "jaipur"
+accountEmail = "st@123"
+accountCity = "delhi"
+let accountState ;// remians undefined
 
+//variable declare krne k 2 tarike one let one var 
+//bs const and let use krna 
+/*
+prefer not use var because of issue in block scope and fuctional scope
 
+*/
 
-console.log(acoountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
