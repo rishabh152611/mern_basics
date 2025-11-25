f17a33d83223d3d93fa5bb7228b7664b52219828
Rishabh Pandey <rishabh22154101@akgec.ac.in>
2025-11-25 02:01:04 +0000

    merging logics

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 "use strict";
 let score = '33abc'
 
-console.log(typeof score)
+// console.log(typeof score)
 /*use of Number(datatype)
  // NULL Number mein convert hoke number deta
  //pure no. nhi hai toh (like "33abc") to NaN milega 
@@ -13,11 +13,11 @@ console.log(typeof score)
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log( valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
 let isLoggedin = 1
 let tonumber = Boolean(isLoggedin)
-console.log(tonumber)
+// console.log(tonumber)
 /* boolean coversions
 1=> true
 ""=> false
@@ -27,3 +27,28 @@ let someNumber = 33
 let toNum = String(someNumber)
 console.log(toNum)
 console.log(typeof toNum)
+//******************** */ Operations****************************************
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue)
+// console.log(2*2)//give multiplication
+// console.log(2**9) //gives 2^2
+
+//*************important operations */
+let str1 = "rishabh"
+let str2 = "pandey"
+// console.log(str1+" "+str2)
+// console.log(1+"2")//give 12
+// console.log(1+Number('2'))//gives 3
+// console.log(1+Number("ab"))//gives Nan
+// console.log("1"+2+2)//gives 122 because string was first
+// console.log(1+2+"2")//gives 32 because number was first
+// //logic of above operations and conversions
+// console.log(+true)//give 1
+// console.log(+"")//gives 0
+
+// let gameCounter = 100
+// gameCounter ++
+// console.log(gameCounter)
+
+
