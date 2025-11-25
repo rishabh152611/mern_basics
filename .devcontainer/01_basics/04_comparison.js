29766889361ae63abf9678134d0082074d2830ad
Rishabh Pandey <rishabh22154101@akgec.ac.in>
2025-11-25 02:37:04 +0000

    datatypes summery

--- .devcontainer/01_basics/04_comparison.js
+++ .devcontainer/01_basics/04_comparison.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("rishabh")
+// console.log("2">1)//"2" is converted to number for comparison
+// console.log("02">1)//again"02" is converted to number
+console.log(null>=0)//give true
+console.log(null==0)//gives false
+//undefined k case mein saara false aata hai
+
+//=== is strict check that checks data type
+console.log("2"===2)//gives false does not convert because data type is different
+
+
---
+++ .devcontainer/01_basics/05_datatypes2.js
@@ -0,0 +1,27 @@
+//************Important notes of datatypes for interview*********************************
+//2 types 
+//1. Primitive
+//2. Non Primitive
+
+ //JavaScript is a dynamically typed language: We dont need to define the datatype when we are initializing or defining 
+ let id = Symbol("123")
+ let anotherId= Symbol("123")
+//  console.log(id==anotherId)//returns false
+//  console.log(id)
+//  console.log(anotherId)
+//Primitive
+//7.
+//string , Number , Boolean , null , undefined , symbol , BigInt 
+
+//Reference type/non primitive : arrays , objects* ,functions 
+const heros=["batman","superman","aquaman","flash"]
+console.log(heros)
+console.table([heros[0],heros[2]])
+//definition of objects
+
+let me={
+name :"rishabh",
+age:34,
+
+
+}
