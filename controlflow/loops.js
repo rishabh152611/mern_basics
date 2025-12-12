54275d66f7d877c9391f92dba240bf43f9533b73
Rishabh Pandey <rishabh22154101@akgec.ac.in>
2025-12-12 09:54:42 +0000

    forloops

--- controlflow/loops.js
+++ controlflow/loops.js
@@ -1,1 +1,14 @@
 //for loop
+let arr=[1,2,3,4,5,6,7,8]
+//function to return if a array is sorter or not
+let ans = true
+    for(let i =0;i<arr.length;i++){
+        if(arr[i]>arr[i+1]){
+            ans= false
+        }
+
+    }
+
+
+
+console.log(ans)
